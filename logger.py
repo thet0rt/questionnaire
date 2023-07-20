18eb812802ae3e08e2814ac8d88bc8a4bd81533b
puzikov <gpuzikov@neuro.net>
2023-07-20 18:07:14 +0300

    refactoring

--- logger.py
+++ logger.py
@@ -2,22 +2,14 @@ import logging
 
 
 def setup_logger(log_file, level=logging.INFO):
-    # Create a logger
     logger = logging.getLogger("my_logger")
     logger.setLevel(level)
-
-    # Create a file handler
     file_handler = logging.FileHandler(log_file)
-
-    # Create a formatter
     formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
     file_handler.setFormatter(formatter)
-
-    # Add the file handler to the logger
     logger.addHandler(file_handler)
-
     return logger
 
 
