e17b7e6edefd8dad789e739e1ddc164f502f869f
puzikov <gpuzikov@neuro.net>
2023-07-20 19:28:55 +0300

    bugfix

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     say_welcome_prompts()
     count = get_count()
     question_list = get_question(count)
+    if not question_list:
+        return print('The game is over due to api error. Please contact with the developer in tg: @thet0rt')
     if CHEAT:
         pprint(question_list)
     ask_questions(question_list)
